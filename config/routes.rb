ae491795797eac845c5d636e6a17d7813a2eaed6
jvz90 <izanoaga@chicagobooth.edu>
2021-12-09 23:31:22 +0000

    Index page workf - Final Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   get("/", { :controller => "users", :action => "index" })
-  
+  get("/users", { :controller => "users", :action => "index" })
   
   
   # Routes for the Comment resource:
